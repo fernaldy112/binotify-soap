efa285363b1d54492f584e497e2ff9712242d750
Radenz <radenrifqirahman@gmail.com>
2022-12-01 19:11:07 +0700

    feat: generate 2 API keys on serve

--- src/main/java/com/binotify/ServicePublisher.java
+++ src/main/java/com/binotify/ServicePublisher.java
@@ -5,6 +5,7 @@ import jakarta.xml.ws.Endpoint;
 public class ServicePublisher {
     public static void main(String[] args) {
         KeyProvider.generate();
+        KeyProvider.generate();
 
         Endpoint.publish(
                 "http://0.0.0.0:80/subscription",
